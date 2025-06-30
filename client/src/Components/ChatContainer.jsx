d1bb434e50f5d6ccb16b2cb99344568264bac2e1
arjun vinod <arjunvinod707@gmail.com>
2025-07-01 02:56:07 +0530

    home completed

--- client/src/Components/ChatContainer.jsx
+++ client/src/Components/ChatContainer.jsx
@@ -3,20 +3,23 @@ import assets, { messagesDummyData } from "../assets/assets";
 import { formatMessageTime } from "../library/utils";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  const scrollEnd = useRef();
 
-    const scrollEnd = useRef()
+   useEffect(() => {
+    // Scroll to the last message
+    scrollEnd.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
-    useEffect(()=>{
 
-        if(scrollEnd.current){
-            scrollEnd.current.scrollIntoView({behavior:"smooth"});
-        }
-    },[])
   return selectedUser ? (
     <div className="h-full overflow-hidden relative backdrop-blur-lg flex flex-col">
       {/* header */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
-        <img src={selectedUser.profilePic} alt="" className="w-8 rounded-full " />
+        <img
+          src={selectedUser.profilePic}
+          alt=""
+          className="w-8 rounded-full "
+        />
         <p className="flex-1 text-lg text-white flex  items-center gap-2 ">
           {selectedUser.fullName}
           <span className=" w-2 h-2 rounded-full bg-green-500"></span>
@@ -59,9 +62,18 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             )}
 
             <div className="text-center text-xs ">
-                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" className="w-7 rounded-full" />
-                <p className="text-gray-500">{formatMessageTime(msg.createdAt)}</p>
- 
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt=""
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
             </div>
           </div>
         ))}
@@ -70,18 +82,33 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
 
         {/* texting */}
 
-        <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
-            <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
-                <input type="text" placeholder="Send a message"  className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"/>
-                <input type="file"  id="image" accept='image/png, image/jpeg' hidden />
-                <label htmlFor="image">
-                    <img src={assets.gallery_icon} alt=""  className="w-5 mr-2 cursor-pointer "/>
-                </label>
-            </div>
-            <img src={assets.send_button} alt=""  className="w-7 cursor-pointer "/>
-
+        <div className="absolute  bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+          <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+            <input
+              type="text"
+              placeholder="Send a message"
+              className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400"
+            />
+            <input
+              type="file"
+              id="image"
+              accept="image/png, image/jpeg"
+              hidden
+            />
+            <label htmlFor="image">
+              <img
+                src={assets.gallery_icon}
+                alt=""
+                className="w-5 mr-2 cursor-pointer "
+              />
+            </label>
+          </div>
+          <img
+            src={assets.send_button}
+            alt=""
+            className="w-7 cursor-pointer "
+          />
         </div>
-
       </div>
     </div>
   ) : (
